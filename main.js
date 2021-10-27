8f21751c90a0483b859ba40b653da2f0ceba1479
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-27 15:23:01 +0200

    richiesta del prompt completa

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ genNumbersArray()
 
 let disclaimer = alert([...numbersArray])
 
+let clock = setTimeout(askNumberToUser, 2000);
+
 // Funzione che genera dei numeri casuali da 1 a 100
 function randomNumbers() {
   return Math.floor(Math.random() * 100);
@@ -15,4 +17,9 @@ function genNumbersArray() {
   for (let i = 0; numbersArray.length < 5; i++) {
     numbersArray.push(randomNumbers())
   }
+}
+
+// Richiesta da parte nostra all'utente di ricordare i numeri
+function askNumberToUser() {
+  prompt('Vediamo se ti ricordi i numeri! scrivi qui i numeri che erano scritti prima!')
 }
