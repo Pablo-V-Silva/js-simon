e5dc5c33fd55a6689d744ad52e1dfb24eee73d3e
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-27 14:54:36 +0200

    alert con i numeri casuali completo

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+// Dichiarazione dell'array vuoto da riempire a ogni ciclo
+let numbersArray = [];
+
+genNumbersArray()
+
+let disclaimer = alert([...numbersArray])
+
 // Funzione che genera dei numeri casuali da 1 a 100
 function randomNumbers() {
   return Math.floor(Math.random() * 100);
 }
 
-let numbersArray = [];
-
+// Generazione dei numeri da inserire all'interno dell'array
+function genNumbersArray() {
+  for (let i = 0; numbersArray.length < 5; i++) {
+    numbersArray.push(randomNumbers())
+  }
+}
